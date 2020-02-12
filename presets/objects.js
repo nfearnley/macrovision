87c619bf1f4b3056e2cccc472935afb9f63f9e2c
Fen Dweller <chemicalcrux@gmail.com>
2020-02-12 10:19:47 -0500

    Add planets

--- presets/objects.js
+++ presets/objects.js
@@ -31,6 +31,25 @@ function makeObject(name, viewInfo) {
     return makeEntity(name, "Object", views);
 }
 
+function makePlanet(name, diameter, mass, image) {
+    return {
+        name: name,
+        constructor: () => makeObject(
+            name,
+            {
+                body: {
+                    height: diameter,
+                    mass: mass,
+                    image: (image === undefined ? {
+                        source: "./media/objects/planet-generic.svg"
+                    } : image),
+                    name: "Body"
+                }
+            }
+        )
+    };
+}
+
 function makeObjects() {
     const results = [];
 
@@ -67,6 +86,17 @@ function makeObjects() {
             }
         )
     });
+
+    results.push(makePlanet("Mercury", math.unit(4879, "km"), math.unit(0.330e24, "kg")));
+    results.push(makePlanet("Venus", math.unit(12104, "km"), math.unit(4.87e24, "kg")));
+    results.push(makePlanet("Earth", math.unit(12756, "km"), math.unit(5.97e24, "kg")));
+    results.push(makePlanet("Moon", math.unit(3475, "km"), math.unit(0.073e24, "kg")));
+    results.push(makePlanet("Mars", math.unit(6792, "km"), math.unit(0.642e24, "kg")));
+    results.push(makePlanet("Jupiter", math.unit(142984, "km"), math.unit(1898e24, "kg")));
+    results.push(makePlanet("Saturn", math.unit(120536, "km"), math.unit(568e24, "kg"), {source: "./media/objects/saturn.svg"}));
+    results.push(makePlanet("Uranus", math.unit(51118, "km"), math.unit(86.8e24, "kg")));
+    results.push(makePlanet("Neptune", math.unit(49528, "km"), math.unit(102e24, "kg")));
+    results.push(makePlanet("Pluto", math.unit(2370, "km"), math.unit(0.0146e24, "kg")));
 
     return results;
 }
