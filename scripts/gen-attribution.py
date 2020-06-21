dfc60c6475a2180d128f35cc83ee2ed61288ecb5
Fen Dweller <chemicalcrux@gmail.com>
2020-06-21 15:33:01 -0400

    Update a script

--- scripts/gen-attribution.py
+++ scripts/gen-attribution.py
@@ -1,15 +1,16 @@
 import os
 import sys
 
-TEMPLATE = """{{ name: "{0}", source: "{1}", nsfw: "false" }},\n"""
+TEMPLATE = """{{ name: "{0}", source: {1}, nsfw: false }},\n"""
 
 if len(sys.argv) < 1:
     print("Usage: {0} [dir]".format(sys.argv[0]))
     sys.exit(64)
 
 result = ""
+source = '"' + sys.argv[2] + '"' if len(sys.argv) > 2 else "null"
 
 for file in os.listdir(sys.argv[1]):
-    result += TEMPLATE.format(file, sys.argv[2])
+    result += TEMPLATE.format(file, source)
 
 print(result)
